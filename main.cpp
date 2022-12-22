a6121fb57cf0e847291a968ef3149cc5d4ca989b
marshtv <marshtv.1972@gmail.com>
2022-12-22 07:17:30 +0300

    Structs composing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,40 @@
 #include <string>
 #include <vector>
 
+enum buildings_type {
+	MAIN_HOUSE,
+	PARENT_HOUSE,
+	GARAGE,
+	SARAY,
+	BANYA
+};
+
+enum room_type {
+	BEDROOM,
+	KITCHEN,
+	BATHROOM,
+	CHILDREN,
+	LIVING
+};
+
+struct room_spec {
+	room_type roomType;
+	float room_square = 10.f;
+};
+
+struct floor_composition {
+	float ceiling_height = 2.7f;
+	int rooms_number = 4;
+
+};
+
+struct building {
+	buildings_type buildingsType;
+	float square_building = 100.f;
+	int floors_number;
+	floor_composition floorComposition;
+};
+
 int main() {
-	
+
 }
