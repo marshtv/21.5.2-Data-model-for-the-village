4c3b63b1c9be30ab0a09f1b54af24fbfe1abed8f
marshtv <marshtv.1972@gmail.com>
2022-12-22 15:49:20 +0300

    Structs complete, main() start

--- main.cpp
+++ main.cpp
@@ -19,24 +19,32 @@ enum room_type {
 	LIVING
 };
 
-struct room_spec {
-	room_type roomType;
+struct room_composition {
+	room_type roomType = BEDROOM;
 	float room_square = 10.f;
 };
 
 struct floor_composition {
 	float ceiling_height = 2.7f;
 	int rooms_number = 4;
-
+	std::vector<room_composition> roomComposes;
 };
 
-struct building {
-	buildings_type buildingsType;
+struct building_composition {
+	buildings_type buildingsType = MAIN_HOUSE;
 	float square_building = 100.f;
 	int floors_number;
-	floor_composition floorComposition;
+	std::vector<floor_composition> floorComposes;
+	bool furnace_with_pipe = true;
+};
+
+struct area_composition {
+	int area_num = 1;
+	float area_square = 200.f;
+	std::vector<building_composition> buildingComposes;
 };
 
 int main() {
-
+	std::vector<area_composition> village;
+	
 }
